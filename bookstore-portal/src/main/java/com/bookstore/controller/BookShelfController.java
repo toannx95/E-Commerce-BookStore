b6109eda5ccd733cf890ee03ba6561aed11923f6
ToanNX <toannx95@gmail.com>
2018-05-04 10:45:58 +0700

    add code for BookDetail

--- bookstore-portal/src/main/java/com/bookstore/controller/BookShelfController.java
+++ bookstore-portal/src/main/java/com/bookstore/controller/BookShelfController.java
@@ -1,5 +1,6 @@
 package com.bookstore.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,18 @@ public class BookShelfController {
 
 		mav.addObject("bookList", bookList);
 		mav.setViewName("bookShelf");
+		return mav;
+	}
+
+	@RequestMapping(value = "bookDetail", method = RequestMethod.GET)
+	public ModelAndView bookDetail(@RequestParam("id") Long id) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("book", bookService.findOne(id));
+
+		List<Integer> qtyList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		mav.addObject("qtyList", qtyList);
+		mav.addObject("qty", 1);
+		mav.setViewName("bookDetail");
 		return mav;
 	}
 
